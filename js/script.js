e1fc72a87eced7830b8fbe6c385188d7b84ebf35
Ronaldo Carvalho <51083282+ronaldopires@users.noreply.github.com>
2021-12-28 16:53:37 -0300

    Envio de email e correções

--- js/script.js
+++ js/script.js
@@ -1,16 +1,33 @@
-$(document).ready(function(){
-    $(window).scroll(function (event) {
-        var sc = $(window).scrollTop();
-        // console.log(sc);
-        if(sc >= 100){
-            $('.navbar').css("background-color","#1B242F");
-        }else{
-            $('.navbar').css("background-color","transparent");
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >= 100) {
+            $('.navbar').css("background-color", "#1B242F");
+            $("#btnTop").fadeIn();
+        } else {
+            $('.navbar').css("background-color", "transparent");
+            $("#btnTop").fadeOut();
         }
     });
 
-})
-$(".navbar-nav .nav-item .nav-link").on("click", function() {
-    $(".navbar-nav .nav-item .nav-link").removeClass("active");
-    $(this).addClass("active");
+    $("#btnTop").on('click', function() {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 400);
+        return false;
+    });
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
+    $(".navbar-nav .nav-item .nav-link").on("click", function() {
+        $(".navbar-nav .nav-item .nav-link").removeClass("active");
+        $(this).addClass("active");
+    });
 });
