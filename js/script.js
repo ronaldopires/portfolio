a3bde92f6c3f30831de0a64b9e368edbf3fcd076
Ronaldo Carvalho <ronaldo.carvalho@hotmail.com>
2021-12-29 17:53:54 -0300

    Correções de layout e efeitos

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
+$(document).ready(function () {
+    //Animate scroll
+    let left = $('.anime-left');
+    let right = $('.anime-right');
+    let top = $('.anime-top');
+    let bottom = $('.anime-bottom');
+    let start = 'anime-start';
+    let offset = $(window).height() * 3/4;
+
+    if ($(window).scrollTop() > 100) {
+        $('.navbar').css("background-color", "#1B242F");
+        $("#btnTop").fadeIn();
+        animeScroll();
+    }
+    $(window).scroll(function () {
+        animeScroll();
         if ($(this).scrollTop() >= 100) {
             $('.navbar').css("background-color", "#1B242F");
             $("#btnTop").fadeIn();
@@ -9,24 +23,42 @@ $(document).ready(function() {
         }
     });
 
-    $("#btnTop").on('click', function() {
+    function animeScroll() {
+        lopFun(left);
+        lopFun(right);
+        lopFun(top);
+        lopFun(bottom);
+    }
+
+    function lopFun(position){
+        let documentTop = $(window).scrollTop();
+        position.each(function () {
+            let itemTop = $(this).offset().top;
+            if (documentTop > (itemTop - offset)) {
+                $(this).addClass(start);
+            } else {
+                $(this).removeClass(start);
+            }
+        });
+    }
+    $("#btnTop").on('click', function () {
         $('body,html').animate({
             scrollTop: 0
         }, 400);
         return false;
     });
-    $("a").on('click', function(event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function() {
-                window.location.hash = hash;
-            });
-        }
-    });
-    $(".navbar-nav .nav-item .nav-link").on("click", function() {
+    // $("a").on('click', function(event) {
+    //     if (this.hash !== "") {
+    //         event.preventDefault();
+    //         var hash = this.hash;
+    //         $('html, body').animate({
+    //             scrollTop: $(hash).offset().top
+    //         }, 800, function() {
+    //             window.location.hash = hash;
+    //         });
+    //     }
+    // });
+    $(".navbar-nav .nav-item .nav-link").on("click", function () {
         $(".navbar-nav .nav-item .nav-link").removeClass("active");
         $(this).addClass("active");
     });
---
+++ php/email.php
@@ -0,0 +1,39 @@
+<?php
+
+if(isset($_POST['email'])){
+
+    $name = addslashes($_POST['name']);
+    $email = addslashes($_POST['email']);
+    $message = addslashes($_POST['message']);
+    $date_start = date('d/m/Y');
+    $hour_start = date('H:i:s');
+
+    $to = "contato@devronaldo.com.br";
+    $subject = "Contato Site Dev Ronaldo";
+    $body = "
+        <html>
+            <p><b>Nome: </b>$name</p>
+            <p><b>E-mail: </b>$email</p>
+            <p><b>Mensagem: </b>$message</p>
+            <p>Este email foi enviado em <b>$date_start</b> às <b>$hour_start</b></p>
+        </html>
+    "; 
+
+    $header = array(
+        'MIME-Version' => '1.0',
+        'Content-type' => 'text/html; charset=UTF-8',
+        'From' => 'contato@devronaldo.com.br',
+        'Reply-To' => $email,
+        'X-Mailer' => 'PHP/' . phpversion()
+    );
+    
+    if(mail($to, $subject, $body, $header)){
+        echo "Email enviado com sucesso!";
+    }else{
+        echo "Erro ao enviar email, tente novamente!";
+    }
+}else{
+    echo "Preencha corretamente os seus dados e tente novamente";
+}
+    
+?>
